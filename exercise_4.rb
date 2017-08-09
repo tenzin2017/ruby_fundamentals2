9d0319bac71a257761f11192252265408076ee5e
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-09 16:18:07 -0400

    exercise_5 calling name once

---
+++ exercise_4.rb
@@ -0,0 +1,9 @@
+def is_word_8_char?(my_word)
+  if my_word.length < 8
+    return false
+  else
+    return true
+end
+end
+
+p is_word_8_char?("lobsang tenzin")
---
+++ exercise_5.rb
@@ -0,0 +1,5 @@
+def greet_backwards(my_name)
+  return "Hello, #{my_name.reverse!}! Welcome Home"
+end
+
+p greet_backwards("Lobsang")
