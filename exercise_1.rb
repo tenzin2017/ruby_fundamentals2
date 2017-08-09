588efa6daeac2c0c912412d82f096a08097b0660
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-09 15:49:07 -0400

    exercise_1 & exercise_2 solved

---
+++ exercise_1.rb
@@ -0,0 +1,6 @@
+
+def double(my_number)
+  return my_number * 2
+end
+
+p double(20)
---
+++ exercise_2.rb
@@ -0,0 +1,9 @@
+def negative?(my_number)
+  if my_number >= 0
+    return true
+  else
+    return false
+end
+end
+
+p negative?(-100)
