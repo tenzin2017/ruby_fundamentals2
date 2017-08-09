8e82383a24d1928d18db113b6170ea1bfb1462c5
Dolma <dolma@Owners-MacBook-Pro.local>
2017-08-09 16:19:47 -0400

    exercise_5 name appears twice in reverse

--- exercise_5.rb
+++ exercise_5.rb
@@ -1,5 +1,5 @@
 def greet_backwards(my_name)
-  return "Hello, #{my_name.reverse!}! Welcome Home"
+  return "Hello, #{my_name.reverse!*2}! Welcome Home"
 end
 
-p greet_backwards("Lobsang")
+p greet_backwards("Sonam")
